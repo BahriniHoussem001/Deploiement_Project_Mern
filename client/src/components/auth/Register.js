c5cf83d399ebd1c18e384cd88bcd2b1a58e6cf83
BahriniHoussem001 <145834601+BahriniHoussem001@users.noreply.github.com>
2025-05-21 22:29:14 +0100

    Update Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -31,7 +31,7 @@ const Register = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/auth/register', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/auth/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
